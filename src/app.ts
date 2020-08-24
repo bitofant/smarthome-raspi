4cba349200ca5fce48e68e9d46eafca64a503640
Joran Tesse <jtesse@hubspot.com>
2020-08-24 17:10:22 -0400

    removed debug logging

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ const log = logger(module);
 import express from 'express';
 const app = express();
 
-log('attaching logger endpoint...');
 app.use('/log/', loggerExpressEndpoint);
 
 app.listen(8080, () => {
